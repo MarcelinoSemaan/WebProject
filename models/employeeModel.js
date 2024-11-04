61663066902ef1c3b482e3ab9e5c25df30fb4286
FoulerTripod363 <113252404+FoulerTripod@users.noreply.github.com>
2024-11-04 22:06:24 +0200

    created employee model

---
+++ models/employeeModel.js
@@ -0,0 +1,23 @@
+class Employee
+{
+    constructor(employeeID, schoolIDFK, employeeName, employeeType, employeeContact)
+    {
+        this.employeeID = employeeID;
+        this.schoolIDFK = schoolIDFK;
+        this.employeeName = employeeName;
+        this.employeeType = employeeType;
+        this.employeeContact = employeeContact;
+    }
+
+    static fromRow(row) {
+        return new Employee(
+            row.employee_id,
+            row.school_school_id,
+            row.employee_name,
+            row.employee_type,
+            row.employee_contact
+        )
+    }
+}
+
+module.exports = Employee;
